9c582f2157d35cad4955ed2d1a913b989d4db6a2
Parnaigon Yangpaksi <JD@Parnaigons-MacBook-Pro.local>
2015-11-19 18:17:56 +0700

    Add gulp-concat and modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     plumber     = require('gulp-plumber'),
-    browserSync = require('browser-sync');
-
+    browserSync = require('browser-sync'),
+    concat      = require('gulp-concat');
 
 // /////////////////////////////////////
 // Set up error hundler
@@ -31,6 +31,17 @@ gulp.task('sass', function() {
 
 
 // /////////////////////////////////////
+// Javascrips task
+// /////////////////////////////////////
+gulp.task('scripts', function() {
+  return gulp.src('./lib/*.js')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('./js/'))
+});
+
+
+// /////////////////////////////////////
 // Browser sync server
 // /////////////////////////////////////
 gulp.task('sync', function(){
@@ -38,6 +49,7 @@ gulp.task('sync', function(){
     server: './'
   });
   gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('./lib/*.js', ['scripts']).on('change', browserSync.reload);;
   gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
@@ -45,4 +57,4 @@ gulp.task('sync', function(){
 // /////////////////////////////////////
 // Default Task
 // /////////////////////////////////////
-gulp.task('default', ['sass', 'sync']);
+gulp.task('default', ['sass', 'scripts', 'sync']);
