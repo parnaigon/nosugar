7e02956981d50432c281eccb14a4a400ccce93b7
Parnaigon Yangpaksi <JD@Parnaigons-MacBook-Pro.local>
2015-11-26 16:01:35 +0700

    Finish phototype of phone and tablet version of the theme

--- js/all.js
+++ js/all.js
@@ -1,4 +1,18 @@
-alert('hello');
+$(document).ready(function($) {
 
-console.log('hello');
-console.log('hello3');
+  var $transformer = $('.transformer'),
+      $menuToggle  = $('.menu'),
+      $closeIcon   = $('#close-icon');
+
+  //Attach event handler when .menu-toggle is clicked
+  $menuToggle.on('click', function() {
+    event.preventDefault();
+    $transformer.toggleClass('is-open');
+  });
+
+  $closeIcon.on('click', function(){
+    event.preventDefault();
+    $transformer.removeClass('is-open');
+  });
+
+});
