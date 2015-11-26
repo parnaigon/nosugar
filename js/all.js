b7bbc563f45165c225f2b28521a60a51083cd0f7
Parnaigon Yangpaksi <JD@Parnaigons-MacBook-Pro.local>
2015-11-26 17:09:54 +0700

    Add back-top-top button to theme

--- js/all.js
+++ js/all.js
@@ -2,9 +2,12 @@ $(document).ready(function($) {
 
   var $transformer = $('.transformer'),
       $menuToggle  = $('.menu'),
-      $closeIcon   = $('#close-icon');
+      $closeIcon   = $('#close-icon'),
+      $backToTop   = $('.back-to-top');
 
-  //Attach event handler when .menu-toggle is clicked
+// /////////////////////////////////////////////////////////
+// Attach event handler when .menu-toggle is clicked
+// ////////////////////////////////////////////////////////
   $menuToggle.on('click', function() {
     event.preventDefault();
     $transformer.toggleClass('is-open');
@@ -14,5 +17,29 @@ $(document).ready(function($) {
     event.preventDefault();
     $transformer.removeClass('is-open');
   });
+
+
+// ////////////////////////////////////////////////////////
+// Back to the top button
+// ///////////////////////////////////////////////////////
+  var amountScrolled = 300;
+
+  $(window).scroll(function() {
+    event.preventDefault();
+  	if ( $(window).scrollTop() > amountScrolled ) {
+  		$backToTop.fadeIn('slow');
+  	} else {
+  		$backToTop.fadeOut('slow');
+  	}
+  });
+
+  $backToTop.on('click', function() {
+    event.preventDefault();
+    $('html, body').animate({
+  		scrollTop: 0
+  	}, 700);
+  	return false;
+  });
+
 
 });
